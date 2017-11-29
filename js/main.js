c8d0002c03b326f63c84cbe15da8923594c4ee05
James MacLeod <James@Jamess-MBP.netgear.com>
2017-11-30 10:45:23 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
 console.log("Up and Running!");
 
 var cards = ["queen", "queen", "king", "king"];
-
 var cardsInPlay = [];
+var cardId;
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardsInPlay[0]);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+    console.log("You found a match!");
   } else {
-    alert("Sorry try again!");
+    console.log("Sorry, try again.");
   }
-}
+};
+
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry try again!");
+    }
+  }
+  checkForMatch;
+};
+
+cardsInPlay.push(cards[cardId]);
+
+flipCard(0);
+
+flipCard(2);
+
+alert("Sorry, try again.");
